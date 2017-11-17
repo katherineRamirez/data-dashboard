28fd52e226664c65c12e6a56f2bd0b42e5f62ef4
katherineRamirez <oeillet8_5@hotmail.com>
2017-11-17 12:56:47 -0300

    respaldo

--- js/app.js
+++ js/app.js
@@ -4,3 +4,39 @@
 
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
+
+var menuHamburguesa = document.getElementById("hamburguesa");
+var menuLocal = document.getElementById("menu_local");
+var ciudad = document.getElementsByClassName("ciudad");
+var contenedor = document.getElementById("contenedor_menu");
+
+ciudad.addEventListener("click", function(){
+
+	var 
+	
+
+})
+
+/*
+menuHamburguesa.addEventListener("click", function(){
+
+	var divMenu = document.createElement("div");
+	var imagen = document.createElement("img");
+	var ul = document.createElement("ul");
+	var listaAgregar = document.createElement("li");
+	listaAgregar.textContent = "Agregar Estudiante";
+	var listaRemover = document.createElement("li");
+	listaRemover.textContent = "Remover Estudiante";
+	var listaSprint = document.createElement("li");
+	listaSprint.textContent = "Agregar Sprint";
+
+	ul.appendChild(listaAgregar);
+	ul.appendChild(listaRemover);
+	ul.appendChild(listaSprint);
+	divMenu.appendChild(imagen);
+	divMenu.appendChild(ul);
+	contenedor.appendChild(divMenu);
+
+})*/
+
+
