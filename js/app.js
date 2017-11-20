35fc97a1542658ba059f170437f3900b94b5527d
katherineRamirez <oeillet8_5@hotmail.com>
2017-11-20 18:13:51 -0300

    version 3

--- js/app.js
+++ js/app.js
@@ -5,7 +5,6 @@
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
 
-var contenedor = document.getElementById("contenedor_menu");
 var hamburguesa = document.getElementById("hamburguesa");
 var equis = document.getElementById("equis");
 var contenedorHamburguesa = document.getElementById("contenedor_menu_hamburguesa");
@@ -69,92 +68,20 @@ hamburguesa.addEventListener("click", function(){
 
 	})
 
-	 var ciudad = document.getElementById('ciudad');
-ciudad.addEventListener("click", function(){
-
-	var selectAño = document.createElement("select");
-	var optgroupAño = document.createElement("optgroup");
-		optgroupAño.setAttribute("label","2014");
-	var turnoAm = document.createElement("option");
-		turnoAm.setAttribute("value", "1");
-	var textoAm = document.createTextNode("Turno Am");
-	var turnoPm = document.createElement("option");
-		turnoPm.setAttribute("value", "2");
-	var textoPm = document.createTextNode("Turno Pm");
-
-	var optgroupAño2 = document.createElement("optgroup");
-		optgroupAño2.setAttribute("label","2015");
-	var turnoAm2 = document.createElement("option");
-		turnoAm2.setAttribute("value", "3");
-	var textoAm2 = document.createTextNode("Turno Am");
-	var turnoPm2 = document.createElement("option");
-		turnoPm2.setAttribute("value", "4");
-	var textoPm2 = document.createTextNode("Turno Pm");
-
-	var optgroupAño3 = document.createElement("optgroup");
-		optgroupAño3.setAttribute("label","2016");
-	var turnoAm3 = document.createElement("option");
-		turnoAm3.setAttribute("value", "5");
-	var textoAm3 = document.createTextNode("Turno Am");
-	var turnoPm3 = document.createElement("option");
-		turnoPm3.setAttribute("value", "6");
-	var textoPm3 = document.createTextNode("Turno Pm");
-
-	var optgroupAño4 = document.createElement("optgroup");
-		optgroupAño4.setAttribute("label","2017");
-	var turnoAm4 = document.createElement("option");
-		turnoAm4.setAttribute("value", "7");
-	var textoAm4 = document.createTextNode("Turno Am");
-	var turnoPm4 = document.createElement("option");
-		turnoPm4.setAttribute("value", "8");
-	var textoPm4 = document.createTextNode("Turno Pm");
-
-	optgroupAño.appendChild(turnoAm);
-	optgroupAño.appendChild(turnoPm);
-	turnoAm.appendChild(textoAm);
-	turnoPm.appendChild(textoPm);
-	selectAño.appendChild(optgroupAño);
-
-	optgroupAño2.appendChild(turnoAm2);
-	optgroupAño2.appendChild(turnoPm2);
-	turnoAm2.appendChild(textoAm2);
-	turnoPm2.appendChild(textoPm2);
-	selectAño.appendChild(optgroupAño2);
-
-	optgroupAño3.appendChild(turnoAm3);
-	optgroupAño3.appendChild(turnoPm3);
-	turnoAm3.appendChild(textoAm3);
-	turnoPm3.appendChild(textoPm3);
-	selectAño.appendChild(optgroupAño3);
-
-	optgroupAño4.appendChild(turnoAm4);
-	optgroupAño4.appendChild(turnoPm4);
-	turnoAm4.appendChild(textoAm4);
-	turnoPm4.appendChild(textoPm4);
-	selectAño.appendChild(optgroupAño4);
-
-	contenedor.appendChild(selectAño);
-})
-
-	var contenedorEstudiantes = document.getElementById("contenedor-estudiantes");
-	var students = document.getElementById("estudiantes");
-students.addEventListener("click", function(){
-
-	var divEstudiantes = document.createElement("div");
-	var botonActivas = document.createElement("button");
-		botonActivas.textContent = "Activas";
-		botonActivas.classList.add("class", "clase_activas");
-	var botonInactivas = document.createElement("button");
-		botonInactivas.textContent = "Inactivas";
-		botonInactivas.classList.add("class", "clase_inactivas");
-	var inputEstudiantes = document.createElement("input");
-
-	divEstudiantes.appendChild(botonActivas);
-	divEstudiantes.appendChild(botonInactivas);
-	divEstudiantes.appendChild(inputEstudiantes);
-	contenedor-estudiantes.appendChild(divEstudiantes);
-
-})
+	 
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
 
 
 
